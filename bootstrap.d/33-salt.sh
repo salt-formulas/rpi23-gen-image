1f6a17a6758c5f071fe49fc44936457bb201494d
Ales Komarek <mail@newt.cz>
2017-12-01 10:43:36 +0100

    Fixed the salt preseeding

--- bootstrap.d/33-salt.sh
+++ bootstrap.d/33-salt.sh
@@ -7,23 +7,25 @@
 
 # Install Salt minion
 if [ "$ENABLE_SALT" = true ] ; then
-  chroot_exec mkdir -p "${ETC_DIR}/apt/sources.list.d"
-  echo "deb http://repo.saltstack.com/apt/debian/8/armhf/${SALT_VERSION} jessie main" >> "${ETC_DIR}/apt/sources.list.d/saltstack.list"
+  mkdir -p "${ETC_DIR}/apt/sources.list.d"
+  printf "deb http://repo.saltstack.com/apt/debian/8/armhf/${SALT_VERSION} jessie main" >> "${ETC_DIR}/apt/sources.list.d/saltstack.list"
 
   wget -O - "https://repo.saltstack.com/apt/debian/8/armhf/${SALT_VERSION}/SALTSTACK-GPG-KEY.pub" | chroot_exec apt-key add -
 
   chroot_exec apt-get -qq -y update
   chroot_exec apt-get -qq -y install salt-minion
-fi
 
-# Configure Salt minion
-if [ "$CONFIGURE_SALT" = true ] ; then
-  echo "id: ${SALT_MINION}" >> "${ETC_DIR}/salt/minion.d/minion.conf"
-  echo "master: ${SALT_MASTER}" >> "${ETC_DIR}/salt/minion.d/minion.conf"
-fi
+  # Configure Salt minion
+  if [ "$CONFIGURE_SALT" = true ] ; then
+    printf "id: ${SALT_MINION}" >> "${ETC_DIR}/salt/minion.d/minion.conf"
+    printf "\nmaster: ${SALT_MASTER}" >> "${ETC_DIR}/salt/minion.d/minion.conf"
+  fi
 
-# Seed Salt minion keys
-if [ "$SEED_SALT" = true ] ; then
-  echo "${SALT_PUBLIC_KEY}" >> "${ETC_DIR}/salt/pki/minion/minion.pub"
-  echo "${SALT_PRIVATE_KEY}" >> "${ETC_DIR}/salt/pki/minion/minion.pem"
+  # Preseed Salt minion keys
+  if [ "$PRESEED_SALT" = true ] ; then
+    mkdir -p "${ETC_DIR}/salt/pki/minion"
+    printf "${SALT_PUB_KEY}" >> "${ETC_DIR}/salt/pki/minion/minion.pub"
+    printf "${SALT_PRIV_KEY}" >> "${ETC_DIR}/salt/pki/minion/minion.pem"
+  fi
+
 fi
